7f73dbf0db7de1c9a8af81aa91032c814ba36802
Ephraim Park <ephiepark@gmail.com>
2022-09-05 15:15:15 -0700

    Make username clickable profilefeed

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -2,6 +2,7 @@ import { Box, CircularProgress, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { withFirebaseApi, WithFirebaseApiProps } from "../Firebase";
 import { TweetWithId, UserInfo } from "../types";
+import { Link } from 'react-router-dom';
 
 const TweetBase = (props: {
   tweet: TweetWithId,
@@ -13,9 +14,9 @@ const TweetBase = (props: {
     });
   }, []);
   return (
-    <Box sx={{margin: 5}}>
+    <Box sx={{ margin: 5 }}>
       <Typography variant="body1" component="div" align="left">
-        {userInfo?.username ?? <CircularProgress />}
+        <Link to={"/user/" + props.tweet.userId}>{userInfo?.username ?? <CircularProgress />}</Link>
       </Typography>
       <Typography variant="body1" component="div" align="left">
         {props.tweet.tweetContent}
